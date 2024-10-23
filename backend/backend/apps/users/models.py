039ae5b976a2b170d8c46dba2b5f3d8b6dfa52b8
Sabbir Ahmmed <ahsabbir103@gmail.com>
2024-10-23 12:06:48 +0600

    some changes made in user manager

--- backend/backend/apps/users/models.py
+++ backend/backend/apps/users/models.py
@@ -1,24 +1,28 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.validators import RegexValidator
 
-class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, first_name, last_name, email, phone = None, password = None):
         if not email:
             raise ValueError("Users must have an email address")
         if not username:
             raise ValueError("Users must have a username")
 
         user = self.model(
-            username=username,
+            first_name = self.first_name,
+            last_name = self.last_name,
             email=self.normalize_email(email),
+            phone = self.phone
+            **extra_fields
         )
         user.set_password(password)  # Hash the password
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password):
-        user = self.create_user(username, email, password)
+    def create_superuser(self, first_name, last_name, email, password):
+        user = self.create_user(first_name, last_name, email, password)
         user.role = 'admin'  # Set the role to admin
         user.is_staff = True
         user.is_superuser = True
@@ -26,17 +30,26 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # Primary key
     USER_ROLES = (
         ('vendor', 'Vendor'),
         ('customer', 'customer'),
-        ('admin', 'Admin'),
     )
 
-    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # Primary key
-    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
+    email_verified = models.BooleanField(default=False)
+    phone_number = models.CharField(
+        max_length=15,
+        validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$')],
+        unique=True,
+        null=True,
+        blank=True
+    )
+    phone_verified = models.BooleanField(default=False)
     # password_hash = models.CharField(max_length=128)
-    role = models.CharField(max_length=6, choices=USER_ROLES, default='buyer')  # Default role
+    role = models.CharField(max_length=6, choices=USER_ROLES, default='customer')  # Default role
     created_at = models.DateTimeField(auto_now_add=True)  # Timestamp for creation
 
     objects = UserManager()
