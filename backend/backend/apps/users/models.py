3c347c1906d69eae44b41fd88e044e68329c8fe3
Sabbir Ahmmed <ahsabbir103@gmail.com>
2024-10-23 12:58:14 +0600

    updated

--- backend/backend/apps/users/models.py
+++ backend/backend/apps/users/models.py
@@ -55,7 +55,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'  # Use username as the unique identifier
-    REQUIRED_FIELDS = ['first_name', 'email', 'phone']  # Email is required, but not the username
+    REQUIRED_FIELDS = ['first_name', 'phone']  # Email is required, but not the username
 
     def __str__(self):
         return self.user_id
