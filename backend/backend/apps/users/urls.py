b780166bc456b9bf478b65dbe1be29a4db1aeff0
Sabbir Ahmmed <ahsabbir103@gmail.com>
2024-10-23 17:24:24 +0600

    updated

--- backend/backend/apps/users/urls.py
+++ backend/backend/apps/users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('register', UserRegisterView.as_view(), name='register'),
     path('login', UserLoginView.as_view(), name='login'),
     path('profiles', GetUser.as_view(), name='getuser'),
-    path('email-verify', VerifyEmail.as_view(), name='verifyemail'),
+    path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
 ]
