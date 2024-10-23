dff429dabdfe3a6558f0fdbd3517ccbe69f3435d
Sabbir Ahmmed <ahsabbir103@gmail.com>
2024-10-23 17:29:16 +0600

    updated smtp server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,6 +107,17 @@ DATABASES = {
 }
 
 
+
+# settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'arifalauddin44@gmail.com'
+EMAIL_HOST_PASSWORD = 'v7@GBi#k.RP'
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
