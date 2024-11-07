74aad9e161e6adb65a38cd970d123752c9507347
Sabbir Ahmmed <ahsabbir103@gmail.com>
2024-11-07 12:23:43 +0600

    updated

--- backend/backend/apps/categories/admin.py
+++ backend/backend/apps/categories/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+# class ProductAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('title',)}
