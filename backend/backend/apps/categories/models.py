119516a59ecdb58fad6a9bd071594b0b5182ffa5
Sabbir Ahmmed <ahsabbir103@gmail.com>
2024-11-07 12:20:08 +0600

    updated

--- backend/backend/apps/categories/models.py
+++ backend/backend/apps/categories/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.utils.text import slugify
 
-from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='subcategories')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
