0c218401b88ff368e079932608773a73cfcf0cc1
Sabbir Ahmmed <ahsabbir103@gmail.com>
2024-11-07 11:52:32 +0600

    updated backend.apps.categories.views

--- backend/backend/apps/categories/urls.py
+++ backend/backend/apps/categories/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet
+from .views import CategoryViewSet#, ProductViewSet
 
 # router = DefaultRouter()
 # router.register(r'categories', CategoryViewSet)
